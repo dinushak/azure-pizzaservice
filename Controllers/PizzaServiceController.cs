edb0e1a187c405a9eccd45baad2f4b311c92824a
Dinusha Kumarasiri <kumarasiri048@gmail.com>
2022-07-01 10:56:03 +0530

    first commit

---
+++ Controllers/PizzaServiceController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaServiceController : ControllerBase
+{
+    
+    [HttpGet(Name = "GetPizzaOrders")]
+    public IEnumerable<PizzaOrder> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new PizzaOrder
+        {
+            Id = Random.Shared.Next(1, 100),
+            PizzaMake = "Cheese",
+            PizzaCount = Random.Shared.Next(1, 5)
+        })
+        .ToArray();
+    }
+}
