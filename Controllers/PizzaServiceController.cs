e75bcb50430807e42785acf56879f996f5efe564
Dinusha Kumarasiri <kumarasiri048@gmail.com>
2022-07-01 12:20:14 +0530

    type array

--- Controllers/PizzaServiceController.cs
+++ Controllers/PizzaServiceController.cs
@@ -10,10 +10,11 @@ public class PizzaServiceController : ControllerBase
     [HttpGet(Name = "GetPizzaOrders")]
     public IEnumerable<PizzaOrder> Get()
     {
+        string[] pizzaType = {"Fruit","Veg","Seafood","Chicken"};
         return Enumerable.Range(1, 5).Select(index => new PizzaOrder
         {
             Id = Random.Shared.Next(1, 100),
-            PizzaMake = "Fruit",
+            PizzaMake = pizzaType[Random.Shared.Next(0, pizzaType.Length-1)],
             PizzaCount = Random.Shared.Next(1, 5)
         })
         .ToArray();
